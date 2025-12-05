6b364a1227cd5dffaf395cda75503121845cb800
navehtz <navehtz@mta.ac.il>
2025-12-05 18:20:52 +0200

    Updated mainForm, Created Gallery item and post components. not finished

---
+++ FacebookMini/CustomComponent/ItemGalleryComponent.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FacebookWinFormsApp.CustomComponent
+{
+    internal partial class ItemGalleryComponent : UserControl
+    {
+        private const int k_TileWidth = 110;
+        private const int k_TileHeight = 130;
+        private const int k_TileMargin = 8;
+
+        private int m_TotalContentHeight;
+        private bool m_IsFavoriteSelectionMode = false;
+
+        public event EventHandler<GalleryItem> ItemClicked;
+        public event EventHandler<GalleryItem> AddToFavoritesRequested;
+
+        public ItemGalleryComponent()
+        {
+            InitializeComponent();
+
+            vScrollBar.Scroll += vScrollBar_Scroll;
+            this.Resize += ItemGalleryComponent_Resize;
+        }
+
+        private void btnAddToFavorites_Click(object sender, EventArgs e)
+        {
+            m_IsFavoriteSelectionMode = true;
+            btnAddToFavorites.Text = "Select item…";
+            Cursor = Cursors.Hand;
+        }
+
+        public void SetItems(IEnumerable<GalleryItem> items)
+        {
+            panelItems.SuspendLayout();
+            panelItems.Controls.Clear();
+
+            int index = 0;
+            int columns = Math.Max(1, panelItems.Width / (k_TileWidth + k_TileMargin));
+
+            foreach (GalleryItem item in items)
+            {
+                Panel tilePanel = createTilePanel(item);
+
+                int col = index % columns;
+                int row = index / columns;
+
+                tilePanel.Location = new Point(
+                    col * (k_TileWidth + k_TileMargin),
+                    row * (k_TileHeight + k_TileMargin));
+
+                panelItems.Controls.Add(tilePanel);
+                index++;
+            }
+
+            int rows = (int)Math.Ceiling(index / (double)columns);
+            m_TotalContentHeight = rows * (k_TileHeight + k_TileMargin);
+
+            updateScrollBar();
+            panelItems.ResumeLayout();
+        }
+
+        private Panel createTilePanel(GalleryItem item)
+        {
+            Panel panel = new Panel();
+            panel.Size = new Size(k_TileWidth, k_TileHeight);
+
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Dock = DockStyle.Top;
+            pictureBox.Height = 90;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox.Image = item.Image;
+
+            Label label = new Label();
+            label.Dock = DockStyle.Fill;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Text = item.Title;
+
+            // make whole tile clickable
+            panel.Tag = item;
+            pictureBox.Tag = item;
+            label.Tag = item;
+
+            panel.Click += onTileClick;
+            pictureBox.Click += onTileClick;
+            label.Click += onTileClick;
+
+            panel.Controls.Add(label);
+            panel.Controls.Add(pictureBox);
+
+            return panel;
+        }
+
+        private void onTileClick(object sender, EventArgs e)
+        {
+            if (sender is Control control && control.Tag is GalleryItem item)
+            {
+                if (m_IsFavoriteSelectionMode)
+                {
+                    // leave selection mode
+                    m_IsFavoriteSelectionMode = false;
+                    btnAddToFavorites.Text = "Add to Favorites";
+                    Cursor = Cursors.Default;
+
+                    // tell outside world “this item should be added to favorites”
+                    AddToFavoritesRequested?.Invoke(this, item);
+                }
+                else
+                {
+                    // normal click behavior
+                    ItemClicked?.Invoke(this, item);
+                }
+            }
+        }
+
+        private void vScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            panelItems.Top = -vScrollBar.Value;
+        }
+
+        private void ItemGalleryComponent_Resize(object sender, EventArgs e)
+        {
+            // when the control is resized we may have more/less columns
+            // so in a real project you’d want to re-layout items here
+            updateScrollBar();
+        }
+
+        private void updateScrollBar()
+        {
+            int visibleHeight = panelItems.Height;
+
+            if (m_TotalContentHeight <= visibleHeight)
+            {
+                vScrollBar.Enabled = false;
+                vScrollBar.Value = 0;
+                panelItems.Top = 0;
+            }
+            else
+            {
+                vScrollBar.Enabled = true;
+                vScrollBar.Minimum = 0;
+                vScrollBar.Maximum = m_TotalContentHeight - visibleHeight;
+                vScrollBar.LargeChange = visibleHeight;
+                if (vScrollBar.Value > vScrollBar.Maximum)
+                {
+                    vScrollBar.Value = vScrollBar.Maximum;
+                }
+                panelItems.Top = -vScrollBar.Value;
+            }
+        }
+    }
+}
---
+++ FacebookMini/CustomComponent/PostComponent.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FacebookWinFormsApp.CustomComponent
+{
+    partial class PostComponent
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Label CaptionLabel;
+        private System.Windows.Forms.Label LikesLabel;
+        private System.Windows.Forms.Label CommentsLabel;
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.CaptionLabel = new System.Windows.Forms.Label();
+            this.LikesLabel = new System.Windows.Forms.Label();
+            this.CommentsLabel = new System.Windows.Forms.Label();
+            this.DateTimeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CommentsPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LikesPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfilePicPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NameLabel
+            // 
+            NameLabel.AutoSize = true;
+            NameLabel.Location = new System.Drawing.Point(91, 24);
+            NameLabel.Name = "NameLabel";
+            NameLabel.Size = new System.Drawing.Size(41, 15);
+            NameLabel.TabIndex = 3;
+            NameLabel.Text = "Name";
+            NameLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // DateTimeLabel
+            // 
+            this.DateTimeLabel.AutoSize = true;
+            this.DateTimeLabel.Location = new System.Drawing.Point(91, 48);
+            this.DateTimeLabel.Name = "DateTimeLabel";
+            this.DateTimeLabel.Size = new System.Drawing.Size(64, 15);
+            this.DateTimeLabel.TabIndex = 4;
+            this.DateTimeLabel.Text = "Date Time";
+            // 
+            // CaptionLabel
+            // 
+            CaptionLabel.AutoSize = true;
+            CaptionLabel.Location = new System.Drawing.Point(22, 91);
+            CaptionLabel.Name = "CaptionLabel";
+            CaptionLabel.Size = new System.Drawing.Size(58, 15);
+            CaptionLabel.TabIndex = 5;
+            CaptionLabel.Text = "Caption...";
+            // 
+            // LikesLabel
+            // 
+            LikesLabel.AutoSize = true;
+            LikesLabel.Location = new System.Drawing.Point(44, 187);
+            LikesLabel.Name = "LikesLabel";
+            LikesLabel.Size = new System.Drawing.Size(46, 15);
+            LikesLabel.TabIndex = 6;
+            LikesLabel.Text = "0 Likes";
+            // 
+            // CommentsLabel
+            // 
+            CommentsLabel.AutoSize = true;
+            CommentsLabel.Location = new System.Drawing.Point(144, 187);
+            CommentsLabel.Name = "CommentsLabel";
+            CommentsLabel.Size = new System.Drawing.Size(77, 15);
+            CommentsLabel.TabIndex = 7;
+            CommentsLabel.Text = "0 Comments";
+            CommentsLabel.Click += new System.EventHandler(this.CommentsLabel_Click);
+            // 
+            // CommentsPictureBox
+            // 
+            this.CommentsPictureBox.ErrorImage = global::FacebookWinFormsApp.Properties.Resources.no_image;
+            this.CommentsPictureBox.Image = global::FacebookWinFormsApp.Properties.Resources.Comments;
+            this.CommentsPictureBox.Location = new System.Drawing.Point(104, 179);
+            this.CommentsPictureBox.Name = "CommentsPictureBox";
+            this.CommentsPictureBox.Size = new System.Drawing.Size(33, 33);
+            this.CommentsPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CommentsPictureBox.TabIndex = 9;
+            this.CommentsPictureBox.TabStop = false;
+            // 
+            // LikesPictureBox
+            // 
+            this.LikesPictureBox.ErrorImage = global::FacebookWinFormsApp.Properties.Resources.no_image;
+            this.LikesPictureBox.Image = global::FacebookWinFormsApp.Properties.Resources.Like;
+            this.LikesPictureBox.InitialImage = global::FacebookWinFormsApp.Properties.Resources.Like;
+            this.LikesPictureBox.Location = new System.Drawing.Point(5, 178);
+            this.LikesPictureBox.Name = "LikesPictureBox";
+            this.LikesPictureBox.Size = new System.Drawing.Size(38, 35);
+            this.LikesPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.LikesPictureBox.TabIndex = 8;
+            this.LikesPictureBox.TabStop = false;
+            // 
+            // ProfilePicPictureBox
+            // 
+            this.ProfilePicPictureBox.ErrorImage = global::FacebookWinFormsApp.Properties.Resources.no_image;
+            this.ProfilePicPictureBox.Image = global::FacebookWinFormsApp.Properties.Resources.Facebook_default_male_avatar1;
+            this.ProfilePicPictureBox.InitialImage = null;
+            this.ProfilePicPictureBox.Location = new System.Drawing.Point(15, 15);
+            this.ProfilePicPictureBox.Name = "ProfilePicPictureBox";
+            this.ProfilePicPictureBox.Size = new System.Drawing.Size(60, 57);
+            this.ProfilePicPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ProfilePicPictureBox.TabIndex = 0;
+            this.ProfilePicPictureBox.TabStop = false;
+            // 
+            // PostController
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CommentsPictureBox);
+            this.Controls.Add(this.LikesPictureBox);
+            this.Controls.Add(CommentsLabel);
+            this.Controls.Add(LikesLabel);
+            this.Controls.Add(CaptionLabel);
+            this.Controls.Add(this.DateTimeLabel);
+            this.Controls.Add(NameLabel);
+            this.Controls.Add(this.ProfilePicPictureBox);
+            this.Name = "PostController";
+            this.Size = new System.Drawing.Size(387, 224);
+            ((System.ComponentModel.ISupportInitialize)(this.CommentsPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LikesPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProfilePicPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ProfilePicPictureBox;
+        private System.Windows.Forms.Label DateTimeLabel;
+        private System.Windows.Forms.PictureBox LikesPictureBox;
+        private System.Windows.Forms.PictureBox CommentsPictureBox;
+    }
+}
