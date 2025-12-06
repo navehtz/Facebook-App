4c275cdc0d9fc8f6ca6d9135df0bef23eda2324c
navehtz <navehtz@mta.ac.il>
2025-12-06 12:23:27 +0200

    Fixed bugs in ui. Seperated logic from ui.

---
+++ FacebookMini/Logic/IFacebookAppLogic.cs
@@ -0,0 +1,13 @@
+﻿using FacebookWrapper.ObjectModel;
+using System.Collections.Generic;
+
+namespace FacebookMini.Logic
+{
+    public interface IFacebookAppLogic
+    {
+        User LoggedInUser { get; }
+        IEnumerable<Post> GetUserPosts();
+        IEnumerable<Album> GetUserAlbums();
+        IEnumerable<Page> GetUserLikedPages();
+    }
+}
