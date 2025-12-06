50772d3c502fc3f9bbe99754d1946f008668473e
navehtz <navehtz@mta.ac.il>
2025-12-06 20:17:14 +0200

    Updated structure of project. Added dummy objects

---
+++ FacebookMini/logic/dummyData/DummyDataLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWinFormsApp.utils;
+using Newtonsoft.Json;
+
+namespace FacebookWinFormsApp.logic.dummyData
+{
+    public static class DummyDataLoader
+    {
+        public static List<DummyUser> LoadDummyFriends()
+        {
+            string path = Constants.sr_DummyFriendsJsonPath;
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("friends.json not found!", path);
+            }
+
+            string json = File.ReadAllText(path);
+
+            return JsonConvert.DeserializeObject<List<DummyUser>>(json);
+        }
+
+        public static List<DummyPost> LoadDummyPosts()
+        {
+            string path = Constants.sr_DummyPostsJsonPath;
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("posts.json not found!", path);
+            }
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<DummyPost>>(json);
+        }
+
+        public static List<DummyPhoto> LoadDummyPhotos()
+        {
+            string path = Constants.sr_DummyPhotosJsonPath;
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException("photos.json not found!", path);
+            }
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<List<DummyPhoto>>(json);
+        }
+    }
+}
---
+++ FacebookMini/logic/dummyData/DummyPhoto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookWinFormsApp.logic.dummyData
+{
+    public class DummyPhoto
+    {
+        public string Id { get; set; }
+        public string AuthorId { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public List<string> Likes { get; set; }
+        public List<DummyComment> Comments { get; set; }
+    }
+}
