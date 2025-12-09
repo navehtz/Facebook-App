c0ad000de43c5dddd9796235b93d84805ee009bf
navehtz <navehtz@mta.ac.il>
2025-12-09 22:17:25 +0200

    Interface for post Tags

---
+++ FacebookMini/logic/postTags/IPostTagsManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookWinFormsApp.logic.postTags
+{
+    internal interface IPostTagsManager
+    {
+        List<string> GetPostTags(string i_PostId);
+        void SetPostTags(string i_PostId, List<string> i_Tags);
+        List<string> GetAllTags();
+    }
+}
