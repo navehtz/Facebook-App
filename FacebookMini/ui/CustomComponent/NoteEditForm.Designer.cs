b78198177858f36c899bc67bb4986166e660dc2b
navehtz <navehtz@mta.ac.il>
2025-12-16 22:20:29 +0200

    Added facebook icon to the app

--- FacebookMini/ui/CustomComponent/NoteEditForm.Designer.cs
+++ FacebookMini/ui/CustomComponent/NoteEditForm.Designer.cs
@@ -35,36 +35,41 @@ namespace FacebookMini.ui.CustomComponent
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBoxNote = new TextBox();
-            this.btnSave = new Button();
-            this.btnCancel = new Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteEditForm));
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxNote
             // 
             this.textBoxNote.Location = new System.Drawing.Point(12, 12);
             this.textBoxNote.Multiline = true;
+            this.textBoxNote.Name = "textBoxNote";
             this.textBoxNote.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBoxNote.Size = new System.Drawing.Size(360, 150);
             this.textBoxNote.TabIndex = 0;
             // 
-            // buttonSave
+            // btnSave
             // 
             this.btnSave.Location = new System.Drawing.Point(216, 180);
+            this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 30);
             this.btnSave.TabIndex = 1;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            this.btnSave.Click += new EventHandler(this.buttonSave_Click);
+            this.btnSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
-            // buttonCancel
+            // btnCancel
             // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCancel.Location = new System.Drawing.Point(297, 180);
+            this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 30);
             this.btnCancel.TabIndex = 2;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
-            this.btnCancel.Click += new EventHandler(this.buttonCancel_Click);
+            this.btnCancel.Click += new System.EventHandler(this.buttonCancel_Click);
             // 
             // NoteEditForm
             // 
@@ -75,12 +80,15 @@ namespace FacebookMini.ui.CustomComponent
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.btnCancel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
+            this.Name = "NoteEditForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Post Note";
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
