21547e831f6b47e97165952e70dd341d2cb70f3b
idoomark23 <idoomark23@gmail.com>
2026-01-15 13:37:18 +0200

    builder desing for pages

---
+++ FacebookMini/Logic/prototypeHelpers/CloneMachine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookMini.logic.prototypeHelpers
+{
+    public static class CloneMachine
+    {
+        public static T DeepClone<T>(this T i_ToClone)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, i_ToClone);
+                stream.Seek(0, SeekOrigin.Begin);
+                T theClone = (T)formatter.Deserialize(stream);
+                stream.Close();
+                return theClone;
+            }
+        }
+    }
+}
---
+++ FacebookMini/ui/PageBuilder/IPageBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookMini.ui
+{
+    public interface IPageBuilder
+    {
+        void Reset();
+        void BuildHeader();
+        void BuildBody();
+        void BindData();
+        Control GetResult();
+    }
+}
