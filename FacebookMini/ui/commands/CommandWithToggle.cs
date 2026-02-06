5f759c65112c3d6ba806e09c7fedf3cec789c870
navehtz <navehtz@mta.ac.il>
2026-02-06 18:25:03 +0200

    Changed architecture to observable command. Still need to implement and adapt in UserMainForm

---
+++ FacebookMini/ui/commands/CommandWithToggle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookMini.ui.commands
+{
+    internal class CommandWithToggle : CommandBase
+    {
+        public CommandWithToggle(ICommand i_ChainedCommand)
+            : base(i_ChainedCommand)
+        { }
+
+        protected override void InternalExecute(object param)
+        {
+            this.Checked = !this.Checked;
+        }
+    }
+}
